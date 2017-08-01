9ddf5681bcee0baad80de91f14abb732c1ed079f
Jocelyn Tan <jocelyntan89@hotmail.com>
2017-08-01 14:15:18 +0800

    done 1 and 2

--- lib/02letter_count.rb
+++ lib/02letter_count.rb
@@ -6,3 +6,23 @@
 # letter_count('banana')
 #
 # > {"b" => 1, "a" => 3, "n" => 2}
+def letter_count(str)
+  # split the string, delimiter ""
+  # get each
+  # loop
+  # create a hash
+  str_arr = str.split('')
+  p str_arr
+  occurence = {}
+
+  str_arr.each do |letter|
+    if occurence [letter]
+      occurence[letter] += 1
+    else
+      occurence[letter] = 1
+    end
+  end
+  p occurence
+end
+
+letter_count('banana')
